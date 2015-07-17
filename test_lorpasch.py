4c9403f8567a7857f019f0f6b233ea4956d0a411
Thomas Levine <_@thomaslevine.com>
2015-07-17 09:58:41 -0400

    test stubs

---
+++ test_lorpasch.py
@@ -0,0 +1,26 @@
+from lorpasch import Lorpasch
+
+def test_init():
+    pass
+
+class TestLorpasch:
+    def setup(self):
+        assert False
+
+    def test_iter():
+        pass
+
+    def test_rollup():
+        pass
+
+    def test_insert():
+        pass
+
+    def test__slice():
+        pass
+
+    def test_slice():
+        pass
+
+    def test_dice():
+        pass
